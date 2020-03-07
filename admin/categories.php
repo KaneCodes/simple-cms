0cc11d66f726d578dea1fa701b5b81bd15fb8817
Kane Codes <kane.suckling@gmail.com>
2020-03-07 18:41:45 +0000

    Crud opps on Admin categories page

--- admin/categories.php
+++ admin/categories.php
@@ -2,103 +2,133 @@
 
 <body>
 
-    <div id="wrapper">
-    
-    <?php include "includes/admin_navigation.php"; ?>
-    
-        <div id="page-wrapper">
+<div id="wrapper">
 
-            <div class="container-fluid">
+<?php include "includes/admin_navigation.php"; ?>
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Categories
-                            <small>Author</small>
-                        </h1>
-                        <div class="col-xs-6">
-                        <?php 
-                        // Check if button submitted
-                        if(isset($_POST['submit'])) {
-                        // Assign Variables
-                        $cat_title = $_POST['cat_title'];
+<div id="page-wrapper">
 
-                        // Validation checks
-                        if($cat_title == "" || empty($cat_title)) {
+    <div class="container-fluid">
 
-                            echo "This field should not be empty";
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Categories
+                    <small>Author</small>
+                </h1>
+                <div class="col-xs-6">
+                <?php 
+                // ADD CATEGORY BUTTON
+                if(isset($_POST['submit'])) {
+                // Assign Variables
+                $cat_title = $_POST['cat_title'];
 
-                        } else {
-                            // Create Query
-                            $query = "INSERT INTO categories(cat_title) ";
-                            $query .= "VALUE('{$cat_title}') ";
-                            // Send Query
-                            $create_category_query = mysqli_query($connection, $query);
-                            // Display error if query fails
-                            if(!$create_category_query) {
+                // Validation checks
+                if($cat_title == "" || empty($cat_title)) {
 
-                                die('QUERY FAILED' . mysqli_error($connection));
-                                
-                                }
-                            }
+                    echo "This field should not be empty";
 
+                } else {
+                    // Create Query
+                    $query = "INSERT INTO categories(cat_title) ";
+                    $query .= "VALUE('{$cat_title}') ";
+                    // Send Query
+                    $create_category_query = mysqli_query($connection, $query);
+                    // Display error if query fails
+                    if(!$create_category_query) {
+
+                        die('QUERY FAILED' . mysqli_error($connection));
+                        
                         }
-                        ?>
+                    }
+                }
+                ?>
 
-                        <!-- Add Category Form -->
-                        <form action="" method="post">
-                            <div class="form-group">
-                                <label for="cat_title">Add Category</label>
-                                <input class="form-control" type="text" name="cat_title">
-                            </div>
-                            <div class="form-group">
-                                <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
-                            </div>
-                        </form>
-                        </div>
-                         <!-- /.Addy Category Form  -->
-
-                         <div class="col-xs-6">
-                         <?php 
-                        // Create query 
-                        $query = "SELECT * FROM categories";
-                        // Send query 
-                        $select_categories = mysqli_query($connection, $query);
-                        ?>
-                        <!-- Categories Table -->
-                         <table class="table table-bordered table-hover">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Category Title</th>
-                                
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <?php 
-                            // Handle results
-                            while($row = mysqli_fetch_assoc($select_categories)) {
-                                // Assign variableS
-                                $cat_id = $row['cat_id'];
-                                $cat_title = $row['cat_title'];
-                                // Insert values into table
-                                echo "<tr>";
-                                echo "<td>{$cat_id}</td>";
-                                echo "<td>{$cat_title}</td>";
-                                echo "</tr>";
-                            }
-                            ?>
-                            </tbody>
-                         </table>
-                         <!-- /.Categories Table -->
-                         </div>
+            <!-- Add Category Form -->
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="cat_title">Add Category</label>
+                        <input class="form-control" type="text" name="cat_title">
                     </div>
+                    <div class="form-group">
+                        <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                    </div>
+                </form>
+
+                <?php 
+                // INCLUDE UPDATE FORM
+                if(isset($_GET['update'])) {
+
+                    $cat_id = $_GET['update'];
+
+                    include "includes/update_categories.php";
+                }
+                
+                ?>
+
+
+
                 </div>
-                <!-- /.row -->
+                
+            
+                
+                <div class="col-xs-6">
+                <!-- Categories Table -->
+                <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Category Title</th>
+                    
+                    </tr>
+                </thead>
+                <tbody>
+
+                <?php 
+                // DISPLAY ALL CATEGORIES QUERY
+                $query = "SELECT * FROM categories";
+                // Send query 
+                $select_categories = mysqli_query($connection, $query);
+                // Handle results
+                while($row = mysqli_fetch_assoc($select_categories)) {
+                    // Assign variables
+                    $cat_id = $row['cat_id'];
+                    $cat_title = $row['cat_title'];
+                    // Insert values into table
+                    echo "<tr>";
+                    echo "<td>{$cat_id}</td>";
+                    echo "<td>{$cat_title}</td>";
+                    echo "<td><a href='categories.php?delete={$cat_id}'</a>Delete</td>";
+                    echo "<td><a href='categories.php?update={$cat_id}'</a>Update</td>";
+                    echo "</tr>";
+                }
+                ?> 
+
+                <?php 
+                // DELETE CATEGORY QUERY
+                if(isset($_GET['delete'])) {
+                // Assign variable
+                $get_cat_id = $_GET['delete'];
+                // Create query
+                $query = "DELETE FROM categories WHERE cat_id = {$get_cat_id}";
+                // Send query
+                $delete_query = mysqli_query($connection, $query);
+                // Refresh page
+                header("Location: categories.php");
+                    
+                }
+                ?>
+                </tbody>
+                </table>
+                <!-- /.Categories Table -->
+                </div>
             </div>
-            <!-- /.container-fluid -->
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
 
 <?php include "includes/admin_footer.php"; ?>
---
+++ admin/includes/update_categories.php
@@ -0,0 +1,48 @@
+<!-- Update Category Form -->
+<form action="" method="post">
+    <div class="form-group">
+        <label for="cat_title">Update Category</label>
+
+        <?php 
+        // GET VALUES FOR UPDATE CATEGORY QUERY
+        if(isset($_GET['update'])) {
+        // Assign variable from URL
+        $cat_id = $_GET['update'];
+        // Create query
+        $query = "SELECT * FROM categories WHERE cat_id = $cat_id";
+        // Send query 
+        $select_categories_id = mysqli_query($connection, $query);
+        // Handle results
+        while($row = mysqli_fetch_assoc($select_categories_id)) {
+        // Assign variables
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+        ?>
+
+        <input value="<?php if(isset($cat_title)){echo $cat_title;} ?>" type="text" class="form-control" name="cat_title">
+                
+        <?php }} ?>
+        <?php 
+        // UPDATE QUERY
+        if(isset($_POST['update_caregory'])) {
+        // Assign variable from URL
+        $the_cat_title = $_POST['cat_title'];
+        // Create query
+        $query = "UPDATE categories SET cat_title = '{$the_cat_title}' WHERE cat_id = {$cat_id} ";
+        // Send query
+        $update_query = mysqli_query($connection, $query);
+
+        if(!$update_query) {
+
+            die("Query failed" . mysqli_error($connection));
+            }
+            }
+
+        ?>
+    
+    
+    </div>
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit" name="update_category" value="Update Category">
+    </div>
+</form>
