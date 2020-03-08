d3eaf627ff2af67d5de27d1d0becf15cd2184a66
Kane Codes <kane.suckling@gmail.com>
2020-03-08 11:49:21 +0000

    Created admin posts page

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -39,10 +39,10 @@
             <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts<i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="posts_dropdown" class="collapse">
                 <li>
-                    <a href="#">View All Post</a>
+                    <a href="posts.php">View All Post</a>
                 </li>
                 <li>
-                    <a href="#">Add Post</a>
+                    <a href="posts.php">Add Post</a>
                 </li>
             </ul>
         </li>
---
+++ admin/posts.php
@@ -0,0 +1,62 @@
+<!-- Header-->
+<?php include "includes/admin_header.php"; ?>
+
+<body>
+
+<div id="wrapper">
+<!-- Navigation -->
+<?php include "includes/admin_navigation.php"; ?>
+
+<div id="page-wrapper">
+
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+            <h1 class="page-header">
+                    Posts
+                    <small>Author</small>
+                </h1>
+                <!-- Posts Table Head -->
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Author</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Image</th>
+                            <th>Tags</th>
+                            <th>Comments</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Kane Codes</td>
+                            <td>Bootstrap</td>
+                            <td>Frameworks</td>
+                            <td>Live</td>
+                            <td>Image</td>
+                            <td>Test Tag</td>
+                            <td>Comment</td>
+                            <td>Date</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+
+
+
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php"; ?>
